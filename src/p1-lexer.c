3a883ddefff3902357c8080efe25c0d01e889a56
Alice Robertson <robe23am@dukes.jmu.edu>
2021-09-13 17:49:52 -0400

    creating repository

---
+++ src/p1-lexer.c
@@ -0,0 +1,50 @@
+/**
+ * @file p1-lexer.c
+ * @brief Compiler phase 1: lexer
+ */
+#include "p1-lexer.h"
+
+// TokenQueue* lex (char* text)
+// {
+//     TokenQueue* tokens = TokenQueue_new();
+
+//     return tokens;
+// }
+
+TokenQueue* lex (char* text)
+{
+    TokenQueue* tokens = TokenQueue_new();
+
+    /* compile regular expressions */
+    Regex* whitespace = Regex_new("^[ \n]");
+    Regex* letter = Regex_new("^[a-z]");
+    Regex* symbol = Regex_new("(|)|+|*");
+    Regex* integer = Regex_new("0|[1-9][0-9]*");
+    Regex* identifiers = Regex_new("[a-zA-Z]([0-9]|_|[a-zA-Z])");
+
+    /* read and handle input */
+    char match[MAX_TOKEN_LEN];
+    while (*text != '\0') {
+
+        /* match regular expressions */
+        if (Regex_match(whitespace, text, match)) {
+            /* ignore whitespace */
+        } else if (Regex_match(letter, text, match)) {
+            /* TODO: implement line count and replace placeholder (-1) */
+            TokenQueue_add(tokens, Token_new(ID, match, -1));
+        } else {
+            Error_throw_printf("Invalid token!\n");
+        }
+
+        /* skip matched text to look for next token */
+        text += strlen(match);
+    }
+
+    /* clean up */
+    Regex_free(whitespace);
+    Regex_free(letter);
+
+    return tokens;
+}
+
+
