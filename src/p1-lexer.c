befe6588956126408d832fc05db7c202bf77fa65
BainAJ <alexander.bain@caci.com>
2021-09-16 02:15:52 -0400

    Passing all but string C unit tests

--- src/p1-lexer.c
+++ src/p1-lexer.c
@@ -18,9 +18,11 @@ TokenQueue* lex (char* text)
     /* compile regular expressions */
     Regex* whitespace = Regex_new("^[ \n]");
     Regex* letter = Regex_new("^[a-z]");
-    Regex* symbol = Regex_new("\(|\)|+|*"); // took the \ from the lab3 doc
+    Regex* symbol = Regex_new("\\(|\\)|\\+|\\*|-|!|%|<|<=|>=|>|==|!=|&&|(\\|\\|)"); // took the \ from the lab3 doc
     Regex* integer = Regex_new("0|([1-9][0-9]*)");
     Regex* identifiers = Regex_new("^[a-zA-Z][a-zA-Z0-9_]*");
+    Regex* hexidecimal = Regex_new("^0x[a-f0-9][a-f0-9]*");
+    Regex* strings = Regex_new();
 
     /* read and handle input */
     char match[MAX_TOKEN_LEN];
@@ -29,6 +31,12 @@ TokenQueue* lex (char* text)
         /* match regular expressions */
         if (Regex_match(whitespace, text, match)) {
             /* ignore whitespace */
+        } else if (Regex_match(hexidecimal, text, match)) {
+            /* TODO: implement line count and replace placeholder (-1) */
+            TokenQueue_add(tokens, Token_new(HEXLIT, match, -1));
+        } else if (Regex_match(strings, text, match)) {
+            /* TODO: implement line count and replace placeholder (-1) */
+            TokenQueue_add(tokens, Token_new(ID, match, -1));
         } else if (Regex_match(identifiers, text, match)) {
             /* TODO: implement line count and replace placeholder (-1) */
             TokenQueue_add(tokens, Token_new(ID, match, -1));
