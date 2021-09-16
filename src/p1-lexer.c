7485f77d63c5f28cf893ed895cb88c98c3b58468
Alice Robertson <robe23am@dukes.jmu.edu>
2021-09-16 18:06:56 -0400

    added methods to check for key/reserved words

--- src/p1-lexer.c
+++ src/p1-lexer.c
@@ -4,11 +4,14 @@
  */
 #include "p1-lexer.h"
 
-/* use this method to check if the found token is a keyword*/
-bool isKey (char* key) 
-{
-    return true;
-}
+/* declare keyword and reserved array */
+const char * keys[] = {"def", "if", "else", "while", "return", "break", "continue", "int", "bool", "void", "true", "false"};
+const char * reserved[] = {"for", "callout", "class", "interface", "extends", "implements", "new", "this", "string", "float", "double", "null"};
+const int keysSize = 12;
+const int reservedSize = 12;
+/*Initialize function*/
+bool isKey (char* key);
+bool isReserved (char* key);
 
 TokenQueue* lex (char* text)
 {
@@ -16,50 +19,59 @@ TokenQueue* lex (char* text)
 
     /* compile regular expressions */
     Regex* whitespace = Regex_new("^[ \n]");
-    Regex* letter = Regex_new("^[a-z]");
+    Regex* letter = Regex_new("^[a-z]"); // this isnt actually being used anywhere from what i can tell
     Regex* symbol = Regex_new("\\(|\\)|\\+|\\*|-|!|%|<|<=|>=|>|==|!=|&&|(\\|\\|)"); // took the \ from the lab3 doc
     Regex* integer = Regex_new("0|([1-9][0-9]*)");
     Regex* identifiers = Regex_new("^[a-zA-Z][a-zA-Z0-9_]*");
     Regex* hexidecimal = Regex_new("^0x[a-f0-9][a-f0-9]*");
-    Regex* strings = Regex_new("^\"[a-zA-Z]*\""); // placeholder
-    Regex* key = Regex_new("^[a-z]*"); // placeholder
+    Regex* strings = Regex_new("^\"[a-zA-Z]*\""); // placeholder, not sure this is correct
 
     /* read and handle input */
     char match[MAX_TOKEN_LEN];
     int line = 1;
+
     while (*text != '\0') { 
 
         /* match regular expressions */
-        if (Regex_match(whitespace, text, match)) {
-            /* ignore whitespace */
+        if (Regex_match(whitespace, text, match)) // ignore whitespace
+        {
             // add to line count
             // I don't know if this actually should go here
             line = line + 1;
-        } else if (Regex_match(hexidecimal, text, match)) {
-            /* TODO: implement line count and replace placeholder (-1) */
+        } 
+        else if (Regex_match(hexidecimal, text, match)) // hex
+        {
             TokenQueue_add(tokens, Token_new(HEXLIT, match, line));
-        } else if (Regex_match(key, text, match)) {
-            /* TODO: implement line count and replace placeholder (-1) */
-            // check to see if the given input is a keyword
-            if (isKey(text))
+        } 
+        else if (Regex_match(strings, text, match)) // strings
+        {
+            TokenQueue_add(tokens, Token_new(STRLIT, match, line));
+        } 
+        else if (Regex_match(identifiers, text, match)) // identifiers
+        { 
+            if (isKey(text)) // check to see if id is a keyword
             {
                 TokenQueue_add(tokens, Token_new(KEY, match, line));
-            } else {
+            } 
+            else if (isReserved(text)) // check to see if id is a reserved word
+            { 
+                Error_throw_printf("Invalid token!\n");
+            } 
+            else // otherwise, it is an identifier
+            { 
                 TokenQueue_add(tokens, Token_new(ID, match, line));
             }
-        } else if (Regex_match(strings, text, match)) {
-            /* TODO: implement line count and replace placeholder (-1) */
-            TokenQueue_add(tokens, Token_new(STRLIT, match, line));
-        } else if (Regex_match(identifiers, text, match)) {
-            /* TODO: implement line count and replace placeholder (-1) */
-            TokenQueue_add(tokens, Token_new(ID, match, line));
-        } else if (Regex_match(integer, text, match)) {
-            /* TODO: implement line count and replace placeholder (-1) */
+        } 
+        else if (Regex_match(integer, text, match)) // integer
+        { 
             TokenQueue_add(tokens, Token_new(DECLIT, match, line));
-        } else if (Regex_match(symbol, text, match)) {
-            /* TODO: implement line count and replace placeholder (-1) */
+        } 
+        else if (Regex_match(symbol, text, match)) // symbol
+        { 
             TokenQueue_add(tokens, Token_new(SYM, match, line));
-        }  else {
+        } 
+        else // invalid token
+        {
             Error_throw_printf("Invalid token!\n");
         }
 
@@ -72,4 +84,38 @@ TokenQueue* lex (char* text)
     Regex_free(letter);
 
     return tokens;
+}
+
+/* use this method to check if the found token is a keyword*/
+bool isKey (char* key)
+{
+    bool isKey = false;
+    int i = 0;
+
+    while (i < keysSize && !isKey)
+    {
+        if (strcmp(key, keys[i]))
+        {
+            isKey = true;
+        }
+    }
+
+    return isKey;
+}
+
+/* use this method to check if the found token is a reserved word*/
+bool isReserved (char* key)
+{
+    bool isReserved = false;
+    int i = 0;
+
+    while (i < reservedSize && !isReserved)
+    {
+        if (strcmp(key, reserved[i]))
+        {
+            isReserved = true;
+        }
+    }
+    
+    return isReserved;
 }
